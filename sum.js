c9d27b09e9a578c485647ce2269e5c5951e4f281
Dave-ctr <83482840+Dave-ctr@users.noreply.github.com>
2023-12-20 10:34:50 +1300

    Intro to Jest Testing

--- sum.js
+++ sum.js
@@ -1,6 +1,43 @@
-function sum( a, b )
+// function sum( a, b )
+// {
+//   return a + b;
+// }
+
+// module.exports = sum;
+
+// function myFunction( input )
+// {
+//   if ( typeof input !== 'number' ) // check if input is invalid)
+//   {
+//     throw new Error( 'Invalid Input' );
+//   }
+// }
+
+// module.exports = myFunction
+
+// function fetchData( callback )
+// {
+//   return new Promise( ( resolve, reject ) =>
+//   {
+//     setTimeout( () =>
+//     {
+
+//       callback( 'peanut butter' );
+//     }, 1000 );
+//   } );
+// }
+
+// module.exports = fetchData
+
+function fetchPromise()
 {
-  return a + b;
+  return new Promise( ( resolve, reject ) =>
+  {
+    setTimeout( () =>
+    {
+      resolve( 'peanut butter' )
+    }, 1000 )
+  } )
 }
 
-module.exports = sum;
+module.exports = fetchPromise
--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,121 @@
-const sum = require( './sum' );
-test( 'adds 1 + 2 to equal 3', () =>
+// const sum = require( './sum' );
+
+// test( 'adds 1 + 2 to equal 3', () =>
+// {
+//   expect( 1 + 2 ).toBe( 3 );
+// } )
+
+// toBe is used for primitive data types
+
+// test( 'two plus two equals four', () =>
+// {
+//   expect( 2 + 2 ).toBe( 4 );
+// } )
+
+// test( 'object assignment', () =>
+// {
+//   const data = { one: 1 };
+//   data[ 'two' ] = 2;
+//   expect( data ).toEqual( { one: 1, two: 2 } );
+// } )
+
+// test( 'null is falsy', () =>
+// {
+//   const n = null;
+//   expect( n ).toBeFalsy();
+// } )
+
+// test( '0 is falsy', () =>
+// {
+//   const n = 0;
+//   expect( n ).toBeFalsy();
+// } )
+
+// test( 'undefined is falsy', () =>
+// {
+//   const n = undefined;
+//   expect( n ).toBeFalsy();
+// } )
+
+// test( 'one is truthy', () =>
+// {
+//   const n = 1;
+//   expect( n ).toBeTruthy();
+// } )
+
+// const myFunction = require( './sum' );
+
+// test( 'throws on invalid input', () =>
+// {
+//   expect(
+//     () =>
+//     {
+//       myFunction( 'hello' ) //'hello' passes because input is invalid; myFunction(3) would fail because input is valid;
+//     }
+//   ).toThrow();
+// } )
+
+// const fetchData = require( './sum' );
+
+// test( 'the data is peanut butter', done =>
+// {
+//   function callback( data )
+//   {
+//     try
+//     {
+//       expect( data ).toBe( 'peanut butter' );
+//       done();
+//     } catch ( error )
+//     {
+//       done( error );
+//     }
+//   }
+//   fetchData( callback );
+// } )
+
+const fetchPromise = require( './sum' );
+
+// test( 'the data is peanut butter', () =>
+// {
+//   return expect( fetchPromise() ).resolves.toBe( 'the data is peanut butter' )
+// } )
+
+// test( 'the fetch fails with an error', () =>
+// {
+//   return expect( fetchPromise() ).rejects.toThrow( 'error' )
+// } )
+
+// test( 'the data is peanut butter', async () =>
+// {
+//   const data = await fetchPromise();
+//   expect( data ).toBe( 'peanut butter' );
+// } )
+
+// const mockCallback = jest.fn(x => 42 + x);
+// mockCallback(0);
+// mockCallback(1);
+// mockCallback(2);
+
+// test( 'mock implementation of a basic function', () =>
+// {
+//   const mock = jest.fn( x => 42 + x );
+//   expect( mock( 1) ).toBe( 43 );
+//   expect( mock ).toHaveBeenCalledWith( 1 );
+// } )
+
+test( 'spying on a method of an object', () =>
 {
-  expect( sum( 1, 2 ) ).toBe( 3 );
+  const video = {
+    play()
+    {
+      return true;
+    }
+  }
+
+  const spy = jest.spyOn( video, 'play' );
+  video.play();
+
+  expect( spy ).toHaveBeenCalled();
+  spy.mockRestore();
+
 } )
